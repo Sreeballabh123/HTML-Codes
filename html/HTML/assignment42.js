0127964c01f90d22bc9bd419fda9d5d5de243365
Sreeballabh123 <85075101+Sreeballabh123@users.noreply.github.com>
2022-09-05 20:26:18 +0530

    Add files via upload

---
+++ html/HTML/assignment42.js
@@ -0,0 +1,18 @@
+console.log("this is javascript tuttorial");
+
+function greet(name){
+    console.log(name + " is a good boy");
+}
+
+let name = "sree";
+let name1 = "balu";
+let name2 = "lalu";
+let name3 = "galu";
+greet(name);
+greet(name1);
+greet(name2);
+greet(name3);
+//console.log(name + " is a good boy");
+//console.log(name1 + " is a good boy");
+//console.log(name2 + " is a good boy");
+//console.log(name3 + " is a good boy");
